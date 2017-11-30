2494e0a36c6d902c34518dcc4801f743e3d9ca5a
Adam Smith <asmith13194@gmail.com>
2017-11-29 18:44:13 -0700

    [chg] create a search and favorites component

---
+++ client/src/components/FavoritesPage.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class FavoritesPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div>
+
+          <h4>Favorites</h4>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default FavoritesPage;
---
+++ client/src/components/SearchPage.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div>
+
+          <h4>Welcome!</h4>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
