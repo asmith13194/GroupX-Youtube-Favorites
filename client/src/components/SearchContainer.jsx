92fbbb4059285f2d0d24ec3be47e60400e8ab9e2
Adam Smith <asmith13194@gmail.com>
2017-11-30 11:48:10 -0700

    [chg] create search results container

---
+++ client/src/components/SearchContainer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import Card from './Card'
+import {
+  addFavorite, } from '../actions.js'
+
+class SearchContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      addFavorite,
+      searchReducer,
+      favoritesReducer, } = this.props;
+
+      console.log(searchReducer);
+
+    return (
+      <div id='search-container'>
+
+        {searchReducer.results.map(video => <Card video={video} />)}
+
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    searchReducer: state.searchReducer,
+    favoritesReducer: state.favoritesReducer, };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    addFavorite, }, dispatch);
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps, null)(SearchContainer));
