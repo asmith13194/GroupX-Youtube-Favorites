db56cba508398f6bb44fc6341d450ffa23d65e0a
Adam Smith <asmith13194@gmail.com>
2017-11-30 17:03:52 -0700

    [chg] create actions for get/add/delete favorites

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,10 +1,39 @@
 module.exports = {
 
+  getFavorite: () => {
+    return dispatch => {
+      dispatch({ type: 'GET_FAVORITE_START'})
+      fetch(`http://localhost:8001/favorites`)
+      .then(res => res.json())
+      .then(
+        data =>  dispatch({ type: 'GET_FAVORITE_SUCCESS', payload: data.items }),
+        err =>  dispatch({ type: 'GET_FAVORITE_FAILURE', payload: err })
+      );
+    }
+  },
+
   addFavorite: (favorite) => {
-    return {
-      type: 'ADD_FAVORITE',
-      payload: favorite
-    };
+    return dispatch => {
+      dispatch({ type: 'ADD_FAVORITE_START'})
+      fetch(`http://localhost:8001/favorites/add`)
+      .then(res => res.json())
+      .then(
+        data =>  dispatch({ type: 'ADD_FAVORITE_SUCCESS', payload: data.items }),
+        err =>  dispatch({ type: 'ADD_FAVORITE_FAILURE', payload: err })
+      );
+    }
+  },
+
+  deleteFavorite: (videoId) => {
+    return dispatch => {
+      dispatch({ type: 'DELETE_FAVORITE_START'})
+      fetch(`http://localhost:8001/favorites/delete/${videoId}`)
+      .then(res => res.json())
+      .then(
+        data =>  dispatch({ type: 'DELETE_FAVORITE_SUCCESS', payload: data.items }),
+        err =>  dispatch({ type: 'DELETE_FAVORITE_FAILURE', payload: err })
+      );
+    }
   },
 
   fetchData: (query) => {
